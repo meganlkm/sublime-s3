0574ae6a9407195c58ae4588ea67598ad47b76d2
Megan <megan.lkm@gmail.com>
2017-09-09 14:17:52 -0500

    move list_objects to st3 plugin class

--- s3_editor.py
+++ s3_editor.py
@@ -10,31 +10,12 @@ class Buckets(object):
         s3 = boto3.client('s3')
         self.buckets = jmespath.search('Buckets[*].Name', s3.list_buckets())
 
-    def get(self):
+    def get(self, index):
+        return self.buckets.index(index)
+
+    def list(self):
         self.buckets.sort()
         return self.buckets
-
-
-def list_bucket_objects(thing):
-    status = [
-        'Running list_buckets_callback...',
-        'thing type: {}'.format(str(type(thing))),
-        'thing: {}'.format(str(thing))
-    ]
-
-    # sublime.status_message(status)
-
-    panel = sublime.active_window().new_file()
-    panel.set_name("Foo")
-    panel.set_scratch(True)
-    panel.run_command('append', {'characters': '\n'.join(status)})
-
-    # panel = sublime.active_window().create_output_panel('Foo')
-    # panel.set_read_only(False)
-    # panel.run_command('append', {'characters': status})
-    # panel.set_read_only(True)
-
-    # sublime.insert(edit, point, string)
 
 
 class S3EditorCommand(sublime_plugin.TextCommand):
@@ -48,8 +29,18 @@ class ListBuckets(sublime_plugin.WindowCommand):
     def run(self):
         sublime.active_window().show_quick_panel(
             self.list(),
-            list_bucket_objects
+            self.list_objects
         )
 
     def list(self):
-        return Buckets().get()
+        return Buckets().list()
+
+    def list_objects(self, thing):
+        status = [
+            'thing: {}'.format(str(thing))
+        ]
+
+        panel = sublime.active_window().new_file()
+        panel.set_name("Foo")
+        panel.set_scratch(True)
+        panel.run_command('append', {'characters': '\n'.join(status)})
